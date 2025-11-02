cd7c1b5530745b318958647d2a284c57fbce5010
Craig Tait <dummy@example.com>
2025-11-02 12:26:52 +0000

    Fixed test case formatting

--- tests/test_solutions.rs
+++ tests/test_solutions.rs
@@ -12,9 +12,9 @@ mod tests {
         assert_eq!(solution1(&mut vec![-1, -3]), 1);
     }
     #[test]
-    fn test_solution2() {
+    fn test_solution3() {
         assert_eq!(solution3(&mut vec![1, 3, 6, 4, 1, 2]), 5);
-        assert_eq!(solution3(&mut   vec![1, 2, 3]), 4);
+        assert_eq!(solution3(&mut vec![1, 2, 3]), 4);
         assert_eq!(solution3(&mut vec![-1, -3]), 1);
     }
 }
